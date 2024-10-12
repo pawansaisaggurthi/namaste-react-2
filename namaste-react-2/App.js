c43e55be812ca96b9e3881189ba5114bac6cb19e
jithendrasaggurthi <pawansaisaggurthi@gmail.com>
2024-10-12 22:34:34 +0530

    Ep -01 second half
    nested elements using  react

--- namaste-react-2/App.js
+++ namaste-react-2/App.js
@@ -1,10 +1,27 @@
-const headingReact = React.createElement(
-    "h1", 
-    { id: "reactHeading", "data-xyz": "abc" },
-    "Hello Reactee"
+
+{/* <div id="parent">
+    <div id="child1">
+        <h1>Im h1 tag</h1>
+        <h2>Im h1 tag</h2>
+    </div>
+    <div id="child2">
+        <h1>Im h1 tag</h1>
+        <h2>Im h1 tag</h2>
+    </div>
+</div> */}
+
+
+//Same using react
+
+
+
+const heading2 = React.createElement("div", { id: "parent" },
+    [React.createElement("div", { id: "child1" },
+        [React.createElement("h1", {}, "Im h1 tag"), React.createElement("h2", {}, "Im h2 tag")]),
+    React.createElement("div", { id: "child2" }, [React.createElement("h1", {}, "Im h1 tag"), React.createElement("h2", {}, "Im h2 tag")])]
 );
 
-console.log('headingReact',headingReact)
+
 
 const reactRoot = ReactDOM.createRoot(document.getElementById("reactroot"));
-reactRoot.render(headingReact);
+reactRoot.render(heading2);
