ca36e6c243e13945a70bcb94eac4a6b4aae64a35
jithendrasaggurthi <pawansaisaggurthi@gmail.com>
2024-10-13 19:16:31 +0530

    Ep-02 Added React using npm package

--- namaste-react-2/App.js
+++ namaste-react-2/App.js
@@ -1,17 +1,6 @@
 
-{/* <div id="parent">
-    <div id="child1">
-        <h1>Im h1 tag</h1>
-        <h2>Im h1 tag</h2>
-    </div>
-    <div id="child2">
-        <h1>Im h1 tag</h1>
-        <h2>Im h1 tag</h2>
-    </div>
-</div> */}
-
-
-//Same using react
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 
 
