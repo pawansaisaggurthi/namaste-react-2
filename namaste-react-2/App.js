8da540101056fac307ae7c7cfd0a9f35694a92f0
jithendrasaggurthi <pawansaisaggurthi@gmail.com>
2024-10-13 22:15:55 +0530

    EP-03 configuration and functional componenets

--- namaste-react-2/App.js
+++ namaste-react-2/App.js
@@ -3,14 +3,35 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 
-
-const heading2 = React.createElement("div", { id: "parent" },
-    [React.createElement("div", { id: "child1" },
-        [React.createElement("h1", {}, "Im h1 tag"), React.createElement("h2", {}, "Im h2 tag")]),
-    React.createElement("div", { id: "child2" }, [React.createElement("h1", {}, "Im h1 tag"), React.createElement("h2", {}, "Im h2 tag")])]
+const heading = React.createElement(
+    "h1",
+    {id:"heading"},
+    "namaste react"
 );
 
 
 
+const title = (
+<h1>Pawan2</h1>
+)
+
+const HeadingComponent2 = () => (
+    <div id="container">
+                {title}
+        <h1>Namste HeadingComponent2</h1>
+    </div>
+    )
+
+const HeadingComponent = () => (
+    <div id="container">
+        <HeadingComponent2/>
+                {title}
+        <h1>Namste</h1>
+    </div>
+    )
+
+
+
+
 const reactRoot = ReactDOM.createRoot(document.getElementById("reactroot"));
-reactRoot.render(heading2);
+reactRoot.render(<HeadingComponent/>);
